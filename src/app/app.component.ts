cd6f15f2bdffc17599d7f2c8c47a225459201dc1
Ema5-Zyon <emajoule@hotmail.fr>
2021-05-03 15:15:56 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+
+let heroCount = 2;
+
+@Component({
+  selector: 'app-root', // Angular l'interprète comme "racine du dossier"
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '';
+  titleStyle = {'font-family' : 'Arial', 'color' : 'red'};
+  //username = 'Anonymous';
+  heroes = [
+    "Batman", 
+    "Catwoman", 
+    "Wonder Woman", 
+    "Iron Man"
+  ];
+  badGuys = [
+    "Joker", 
+    "Thor", 
+    "Ant-Man", 
+    "Hulk", 
+    "Bucky"
+  ];
+  manager = '';
+
+  manage(bad?: string, add?: string){
+    if(add == 'add'){
+      if(bad == 'bad'){
+        this.badGuys.push(this.manager);
+      }else{
+        this.heroes.push(this.manager);
+        this.title = 'La tour des ' + this.heroes.length + ' héros';
+      }
+    }else{
+      if(bad == 'bad'){
+        const index: number = this.badGuys.indexOf(this.manager);
+        if (index !== -1){
+          this.badGuys.splice(index, 1);
+        }else{
+        const index: number = this.heroes.indexOf(this.manager);
+          if (index !== -1){
+          this.heroes.splice(index, 1);
+          }
+        }
+     
+      }
+    }
+}
+  /*addHero(){
+    this.heroes.push(this.manager);
+    this.title = 'La tour des ' + this.heroes.length + ' héros';
+  }
+  removeHero(){
+    const index: number = this.heroes.indexOf(this.manager);
+    if (index !== -1){
+      this.heroes.splice(index, 1);
+      this.title = 'La tour des ' + this.heroes.length + ' héros';
+    }
+  } 
+*/
+  
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Import
+import { FormsModule } from '@angular/forms';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, 
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
